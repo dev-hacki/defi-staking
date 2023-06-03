d9d41e267d1bf65705d6ee1ee58dfec21a9577fa
Joiner David Cantillo Camargo <131327866+dev-hacki@users.noreply.github.com>
2023-06-03 07:30:30 -0500

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "bootstrap/dist/css/bootstrap.css"; // Bring in Bootstrap.
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById("root")
 );
